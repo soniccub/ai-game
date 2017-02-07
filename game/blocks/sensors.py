619fb89b2bc7e419aaa5cdc76eda7585e0ea4c55
soniccub <minecraftismyliferunescape@gmail.com>
2017-02-07 14:57:51 -0600

    more updates

--- game/blocks/sensors.py
+++ game/blocks/sensors.py
@@ -12,8 +12,20 @@ class Sensor:
         self.world = self.creature.creatures.main.world
         self.coords = coords
         self.direction = direction
+        self.type = type
+        self.type_set(self.type)
 
     def sense(self):
+        object_list = []
+        if self.sensor.world_object_see:
+            object_list.append(self.object_sense())
+
+
+
+        return object_list
+
+
+    def object_sense(self):
         direction_facing = self.direction + self.creature.direction
 
         objects_around = self.world.space_near([self.creature.position[0] + math.cos(
@@ -30,33 +42,64 @@ class Sensor:
                                   angle_measure([i[5], i[4]]),
                                   angle_measure([i[7], i[6]]),
                                   i[8],
-                                  i[9]
-                                  ])
+                                  i[9]])
         objects_seen = []
         for i in object_angles:
             for ii in i:
                 if ii - self.direction < 180 and ii - self.direction > 0:
-                    self.objects_seen.append(i[5], i[6], angle_measure([i[6][1], i[6][0]])), i[9]
+                    self.objects_seen.append(i[5], i[6], angle_measure([i[6][1], i[6][0]]), 0, "object")
+                    ### 0 = object type
+                    ### i[5] is object itself
+                    ### i[6] is position relitive to sensor
+                    ### next takes angle relitive to sensor
+
+        return objects_seen
+
+
+
+    def type_set(self, type):
+
+        if type == "GeneralSensor":
+            self.sensor = GeneralSensor
+        elif type == "CreatureSensor":
+            self.sensor = CreatureSensor
+        elif type == "ObstacleSensor":
+            self.sensor = ObstacleSensor
+        elif type == "FoodSensor":
+            self.sensor = FoodSensor
+
+        else:
+            self.sensor = GeneralSensor
+            print("sensor name not correct", self)
+
+
+
+
 
 class GeneralSensor:
-    def __init__(self, creature, coords, direction):
+    world_object_see = True
+    def __init__(self):
         pass
-
+    def objects_to_input(self, objects_seen):
+        pass
 
 
 
 
 class CreatureSensor:
-    def __init__(self, creature):
+    world_object_see = False
+    def __init__(self):
         pass
 
 
 class ObstacleSensor:
-    def __init__(self, creature):
+    world_object_see = True
+    def __init__(self):
         pass
 
 class FoodSensor:
-    def __init__(self, creature):
+    world_object_see = False
+    def __init__(self):
         pass
 
 
